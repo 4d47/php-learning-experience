3925d061652436c2f73398b968c652e373bc8b8a
Mathieu Gagnon <mathieu@gagnon.name>
2013-12-30 15:48:47 -0500

    Fix array_search test for hhvm

--- tests/Function/ArraySearchTest.php
+++ tests/Function/ArraySearchTest.php
@@ -4,11 +4,14 @@ class Function_ArraySearchTest extends PHPUnit_Framework_TestCase
 {
     public function testNull()
     {
-        $this->setExpectedException('PHPUnit_Framework_Error',
-            version_compare(PHP_VERSION, '5.3', '>=')
-            ? 'array_search() expects parameter 2 to be array, null given'
-            : 'array_search(): Wrong datatype for second argument'
-        );
+        if (defined('HHVM_VERSION')) {
+            $msg = 'Invalid operand type was used: array_search expects array(s)';
+        } else if (version_compare(PHP_VERSION, '5.3', '>=')) {
+            $msg = 'array_search() expects parameter 2 to be array, null given';
+        } else {
+            $msg = 'array_search(): Wrong datatype for second argument';
+        }
+        $this->setExpectedException('PHPUnit_Framework_Error', $msg);
         $this->assertFalse(array_search('a', null));
     }
 }
