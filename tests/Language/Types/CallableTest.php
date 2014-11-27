7d7997b568bfad4fb19e56ea5e786a3a714cc27a
Mathieu Gagnon <mathieu@gagnon.name>
2014-11-27 11:21:57 -0500

    Fix CallableTest, never forget about the past

--- tests/Language/Types/CallableTest.php
+++ tests/Language/Types/CallableTest.php
@@ -4,7 +4,7 @@ class Language_Types_CallableTest extends PHPUnit_Framework_TestCase
 {
     public function testPrivateMethod()
     {
-        $this->assertTrue(call_user_func(['Language_Types_CallableTest', 'one']));
+        $this->assertTrue(call_user_func(array('Language_Types_CallableTest', 'one')));
         $this->assertTrue(call_user_func('Language_Types_CallableTest::one'));
         $this->assertTrue(call_user_func('self::one'));
         $this->assertTrue(call_user_func('static::one'));
