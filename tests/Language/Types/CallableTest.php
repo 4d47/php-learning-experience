70ef45a32c614f46d128db8a0b5ba29162c551cb
Mathieu Gagnon <mathieu@gagnon.name>
2014-11-27 11:39:54 -0500

    Fix for CallableTest
    Notation self::method does not work on 5.2

--- tests/Language/Types/CallableTest.php
+++ tests/Language/Types/CallableTest.php
@@ -6,8 +6,11 @@ class Language_Types_CallableTest extends PHPUnit_Framework_TestCase
     {
         $this->assertTrue(call_user_func(array('Language_Types_CallableTest', 'one')));
         $this->assertTrue(call_user_func('Language_Types_CallableTest::one'));
-        $this->assertTrue(call_user_func('self::one'));
-        $this->assertTrue(call_user_func('static::one'));
+
+        if (version_compare(PHP_VERSION, '5.3', '>=')) {
+            $this->assertTrue(call_user_func('self::one'));
+            $this->assertTrue(call_user_func('static::one'));
+        }
     }
 
     private static function one()
